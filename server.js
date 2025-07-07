6be6859339097530bedee90358f6ff835ea70c74
Tarik Moody <81526818+tmoody1973@users.noreply.github.com>
2025-07-07 12:42:19 -0500

    Setup .env file support for API keys

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
